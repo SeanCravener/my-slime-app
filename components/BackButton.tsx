654278761b07c9edd8b3ea7ec5e61d1b8cfce9b1
Sean Cravener <scravenerpat@gmail.com>
2025-07-22 14:09:48 -0400

    Had an issue testing custom back button on android. Changing onPress to onPressIn fixes issue, but I'm not sure if this is ideal. Will need to look into later.

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -9,6 +9,9 @@ interface BackButtonProps {
   disabled?: boolean;
 }
 
+// Was having issues with button not doing anything when testing in Android
+// Changing onPress to onPressIn fixees issue, but it is not ideal.
+// TODO: Make sure onPressIn won't cause issues or find fix for onPress not working.
 export const BackButton: React.FC<BackButtonProps> = ({
   size = "md",
   variant = "outline",
@@ -27,7 +30,7 @@ export const BackButton: React.FC<BackButtonProps> = ({
     <Button
       variant={variant}
       size={size}
-      onPress={handlePress}
+      onPressIn={handlePress}
       isDisabled={disabled}
       className="w-10 h-10 rounded-lg p-1 ml-2 mr-2"
     >
