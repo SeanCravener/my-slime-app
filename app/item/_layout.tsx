1e6a29872f449ac4fab4954d6f15c98da54f008a
Sean Cravener <scravenerpat@gmail.com>
2025-07-22 14:28:27 -0400

    Updated some option settings in new item layout file, mostly just [id]. Managing header options for that screen in the file directly, will need to come back and update other screen options for this file. Also added item stack to main app layout file.

--- app/item/_layout.tsx
+++ app/item/_layout.tsx
@@ -1,21 +1,17 @@
-// app/item/_layout.tsx
 import React from "react";
 import { Stack } from "expo-router";
 
+// TODO: Figure out optimal settings for screen options, i.e. headershown, presentation, etc.
+// TODO: Decide if this layout screen is needed and if I'm fine without it.
 export default function ItemLayout() {
   return (
     <Stack>
-      <Stack.Screen
-        name="[id]"
-        options={{
-          headerShown: false, // Let the component handle its own header
-        }}
-      />
+      <Stack.Screen name="[id]" />
       <Stack.Screen
         name="[id]/edit"
         options={{
           headerShown: false,
-          presentation: "modal", // Nice for edit screens
+          presentation: "modal",
         }}
       />
       <Stack.Screen
