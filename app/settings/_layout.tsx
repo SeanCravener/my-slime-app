03d558c4cf31eaeea623a221a6616141f4c3e92c
Sean Cravener <scravenerpat@gmail.com>
2025-07-23 18:43:41 -0400

    Added new screens to settings layout file. Might move these around later, like moving edit-profile to go with the profile screen.

---
+++ app/settings/_layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+// TODO: Figure out optimal settings for screen options, i.e. headershown, presentation, etc.
+// TODO: Decide if this layout screen is needed and if I'm fine without it.
+export default function SetttingsLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="advanced"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="change-password"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="edit-profile"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
